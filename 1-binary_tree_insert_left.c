c1489b221b0b09a14124739b0321e6d4ad5954f4
Mohamed Kamal <ku18mz@gmail.com>
2024-01-04 08:38:40 +0200

    check for parent in separate line

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,18 +13,19 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
+	if (parent == NULL)
+		return (NULL);
+
 	new_node = binary_tree_node(parent, value);
-	if (new_node == NULL || parent == NULL)
+	if (new_node == NULL)
 		return (NULL);
 
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
-		parent->left = new_node;
 	}
-	else
-		parent->left = new_node;
+	parent->left = new_node;
 
 	return (new_node);
 }
