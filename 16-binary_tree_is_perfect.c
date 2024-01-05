cba209da08a9f09b4fe79693148c8543035ec97d
Mohamed Kamal <ku18mz@gmail.com>
2024-01-05 02:47:43 +0200

    change fun logic

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -27,12 +27,20 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t height_left, height_right;
-
 	if (tree == NULL)
 		return (0);
 
-	height_left = binary_tree_height(tree->left);
-	height_right = binary_tree_height(tree->right);
-	return (height_left == height_right);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+
+	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+	{
+		if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
+			return (1);
+	}
+
+	return (0);
 }
